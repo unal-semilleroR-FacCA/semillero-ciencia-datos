69d63637fc8e0b042da2912c3c63d71576c38591
Edimer <ejaramij@contratista.epm.co>
2024-07-08 17:27:09 -0500

    Agregando ejemplos

---
+++ code-example/ejemplos-r-py-202401/01-ejemplo-esquisse.R
@@ -0,0 +1,9 @@
+# Cargando bibliotecas
+library(readr)
+library(esquisse)
+
+# Importando datos
+datos <- read_csv("datos/colocaciones_créditos_agropecuarios_2024.csv")
+
+# Usando esquisse
+esquisser(viewer = "browser")
